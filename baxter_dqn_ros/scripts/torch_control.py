6625570f7148fe88cb6e3f79b4cceece1988a1fe
Thomas Jack Power <tpower@umich.edu>
2020-11-11 09:23:41 -0500

    refactor

--- baxter_dqn_ros/scripts/torch_control.py
+++ baxter_dqn_ros/scripts/torch_control.py
@@ -7,16 +7,9 @@ import rospy
 import rospkg
 import sys
 
-from gazebo_msgs.srv import (
-SpawnModel,
-DeleteModel
-)
+from gazebo_msgs.srv import SpawnModel, DeleteModel
 
-from geometry_msgs.msg import (
-Pose,
-Point,
-Quaternion,
-)
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 
 from baxter_robot_class import BaxterManipulator
@@ -32,36 +25,36 @@ def load_gazebo_models():
 	# Load Table SDF
 	table_xml = ''
 	with open (model_path + "cafe_table/model.sdf", "r") as table_file:
-		table_xml=table_file.read().replace('\n', '')
+		table_xml = table_file.read().replace('\n', '')
 		
 	# Load objects URDF
 	xml = {}
 	pose = {}
-	for i in xrange(1,10):
-		xml["object" + str(i)]  = ''
+	for i in range(1,10):
+		xml["object" + str(i)] = ''
 		pose["object" + str(i)] = Pose(position=Point(x=-3.0+i*0.1, y=0.0, z= 0.0))
 	
 	# Red models
 	with open (model_path + "block_r/model.urdf", "r") as object1_file:
-			xml["object1"]=object1_file.read().replace('\n', '')
+			xml["object1"] = object1_file.read().replace('\n', '')
 	with open (model_path + "sphere_r/model.urdf", "r") as object2_file:
-			xml["object2"]=object2_file.read().replace('\n', '')
+			xml["object2"] = object2_file.read().replace('\n', '')
 	with open (model_path + "cylinder_r/model.urdf", "r") as object3_file:
-			xml["object3"]=object3_file.read().replace('\n', '')
+			xml["object3"] = object3_file.read().replace('\n', '')
 	# Green models		
 	with open (model_path + "block_g/model.urdf", "r") as object4_file:
-			xml["object4"]=object4_file.read().replace('\n', '')
+			xml["object4"] = object4_file.read().replace('\n', '')
 	with open (model_path + "sphere_g/model.urdf", "r") as object5_file:
-			xml["object5"]=object5_file.read().replace('\n', '')
+			xml["object5"] = object5_file.read().replace('\n', '')
 	with open (model_path + "cylinder_g/model.urdf", "r") as object6_file:
-			xml["object6"]=object6_file.read().replace('\n', '')
+			xml["object6"] = object6_file.read().replace('\n', '')
 	# Blue models		
 	with open (model_path + "block_b/model.urdf", "r") as object7_file:
-			xml["object7"]=object7_file.read().replace('\n', '')
+			xml["object7"] = object7_file.read().replace('\n', '')
 	with open (model_path + "sphere_b/model.urdf", "r") as object8_file:
-			xml["object8"]=object8_file.read().replace('\n', '')
+			xml["object8"] = object8_file.read().replace('\n', '')
 	with open (model_path + "cylinder_b/model.urdf", "r") as object9_file:
-			xml["object9"]=object9_file.read().replace('\n', '')
+			xml["object9"] = object9_file.read().replace('\n', '')
 
 	# Spawn Table SDF
 	rospy.wait_for_service('/gazebo/spawn_sdf_model')
@@ -69,18 +62,18 @@ def load_gazebo_models():
 		spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 		resp_sdf = spawn_sdf("cafe_table", table_xml, "/",
 				         table_pose, table_reference_frame)
-	except rospy.ServiceException, e:
+	except rospy.ServiceException as e:
 		rospy.logerr("Spawn SDF service call failed: {0}".format(e))
 		
 	# Spawn object URDF
-	for i in xrange(1,10):
+	for i in range(1,10):
 		rospy.wait_for_service('/gazebo/spawn_urdf_model')
 		try:
 			spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 			resp_urdf = spawn_urdf("object"+str(i), xml["object"+str(i)], "/",
 						         pose["object"+str(i)], object_reference_frame)
 						         
-		except rospy.ServiceException, e:
+		except rospy.ServiceException as e:
 			rospy.logerr("Spawn URDF service call failed: {0}".format(e))
 		
 def delete_gazebo_models():
@@ -89,7 +82,7 @@ def delete_gazebo_models():
 		delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 		resp_delete = delete_model("cafe_table")
 		resp_delete = delete_model("object")
-	except rospy.ServiceException, e:
+	except rospy.ServiceException as e:
 		rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
 		
